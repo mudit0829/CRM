2c9431ac60969b6c1e9b1dc0f53c2c447602e4a3
mudit0829 <mekasutechnology@gmail.com>
2025-09-16 10:36:59 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,40 +2,13 @@ const { DataTypes } = require('sequelize');
 const db = require('../config/database');
 
 const User = db.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false
-  },
-  password_hash: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  first_name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  last_name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  role: {
-    type: DataTypes.ENUM('admin','user','manager'),
-    defaultValue: 'user'
-  },
-  phone: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  is_active: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true
-  }
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  password_hash: { type: DataTypes.STRING, allowNull: false },
+  first_name: { type: DataTypes.STRING, allowNull: false },
+  last_name: { type: DataTypes.STRING, allowNull: false },
+  role: { type: DataTypes.ENUM('admin','user','manager'), defaultValue: 'user' },
+  phone: { type: DataTypes.STRING },
+  is_active: { type: DataTypes.BOOLEAN, defaultValue: true }
 });
 
 module.exports = User;
