a6ce7fbc7aa8a7ba0eebd0e404036b565b0e0310
mudit0829 <mekasutechnology@gmail.com>
2025-09-19 10:22:58 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const User = sequelize.define('User', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   username: { type: DataTypes.STRING, unique: true, allowNull: false },
   password: { type: DataTypes.STRING, allowNull: false },
+  role: { type: DataTypes.STRING, allowNull: false, defaultValue: 'user' } // 'admin' or 'user'
 });
 
 module.exports = User;
